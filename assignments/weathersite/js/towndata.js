5c64da4dc298d1e2269bf59e304d5ea252a4b0fb
isahopkins16 <hop16002@byui.edu>
2019-03-19 22:38:21 -0600

    lesson 11

---
+++ assignments/weathersite/js/towndata.js
@@ -0,0 +1,74 @@
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json'; 
+
+var request = new XMLHttpRequest(); 
+
+request.open('GET', requestURL); 
+
+request.responseType = 'json'; 
+request.send(); 
+
+request.onload = function() { 
+var towndata = request.response; 
+gettowndata(towndata) 
+} 
+
+function gettowndata(jsonObj){ 
+var towns = jsonObj["towns"]; 
+
+    for (var i= 0; i < towns.length; i++){
+        var tName = towns[i].name;
+        if (tName === "Preston") {
+            var tName = document.createElement('h5');
+            tName.textContent = towns[i].name;
+            var tMotto = document.createElement('h6');
+            tMotto.textContent = towns[i].motto;
+            var tYear = document.createElement('h6');
+            tYear.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            var tPopulation = document.createElement('h6');
+            tPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
+            var tRain = document.createElement('h6');
+            tRain.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
+            document.getElementById("prestondata").appendChild(tName); 
+            document.getElementById("prestondata").appendChild(tMotto); 
+            document.getElementById("prestondata").appendChild(tYear); 
+            document.getElementById("prestondata").appendChild(tPopulation); 
+            document.getElementById("prestondata").appendChild(tRain); 
+        }
+       
+        else if (tName === "Soda Springs"){
+            var tName = document.createElement('h5');
+            tName.textContent = towns[i].name;
+            var tMotto = document.createElement('h6');
+            tMotto.textContent = towns[i].motto;
+            var tYear = document.createElement('h6');
+            tYear.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            var tPopulation = document.createElement('h6');
+            tPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
+            var tRain = document.createElement('h6');
+            tRain.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
+            document.getElementById("sodaspringsdata").appendChild(tName); 
+            document.getElementById("sodaspringsdata").appendChild(tMotto); 
+            document.getElementById("sodaspringsdata").appendChild(tYear); 
+            document.getElementById("sodaspringsdata").appendChild(tPopulation); 
+            document.getElementById("sodaspringsdata").appendChild(tRain);
+        }
+        
+        else if (tName === "Fish Haven"){
+            var tName = document.createElement('h5');
+            tName.textContent = towns[i].name;
+            var tMotto = document.createElement('h6');
+            tMotto.textContent = towns[i].motto;
+            var tYear = document.createElement('h6');
+            tYear.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            var tPopulation = document.createElement('h6');
+            tPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
+            var tRain = document.createElement('h6');
+            tRain.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
+            document.getElementById("fishhavendata").appendChild(tName); 
+            document.getElementById("fishhavendata").appendChild(tMotto); 
+            document.getElementById("fishhavendata").appendChild(tYear); 
+            document.getElementById("fishhavendata").appendChild(tPopulation); 
+            document.getElementById("fishhavendata").appendChild(tRain);
+        }
+    }
+} 
---
+++ assignments/weathersite/js/townevents.js
@@ -0,0 +1,49 @@
+var sectionte=document.querySelector('section.townevents');
+var requestURL='https://byui-cit230.github.io/weather/data/towndata.json';
+var request= new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType='json';
+request.send();
+request.onload = function() {
+var townEvents = request.response;
+  showTownEvents(townEvents);
+}
+
+function showEvents(jsonObj){
+    var eventsContainer = document.createElement('div');
+    eventsContainer.setAttribute('class', 'mycontainer');
+    var townevents = document.createElement('div');
+    var title = document.createElement('eventstitle');
+    title.textContent = "Local Events";
+    townevents.appendChild(title);
+                
+    var townName=jsonObj['towns'];
+    for (var i = 0; i < townName.length; i++) {
+        var correctName = townName[i].name;
+        if (correctName == "Preston"){ 
+            for (var j = 0; j < townName[i].events.length; j++){
+                var events = document.createElement('p');
+                events.textContent = townName[i].events[j];
+                townevents.appendChild(events);
+            } continue; 
+        }
+        else if (correctName == "Soda Springs"){ 
+            for (var j = 0; j < townName[i].events.length; j++){
+                var events = document.createElement('s');
+                events.textContent = townName[i].events[j];
+                townevents.appendChild(events);
+            } continue; 
+        }
+        else if (correctName == "Fish Haven"){ 
+            for (var j = 0; j < townName[i].events.length; j++){
+                var events = document.createElement('f');
+                events.textContent = townName[i].events[j];
+                townevents.appendChild(events);
+            } continue;  
+        } 
+        continue;
+    }
+    
+    myContainer.appendChild(townevents);
+    sectionte.appendChild(eventsContainer);
+}
